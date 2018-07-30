ee5f642de4c75e31ece3259f8011c7a6e39340a4
Layth <laythq@gmail.com>
2018-07-30 17:28:29 +0100

    Validates E-mail (length, characters)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+  format: { with: VALID_REGEX }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,23 @@ RSpec.describe User, type: :model do
       @user.email = 'a' * 244 + '@example.com'
       expect(@user).not_to be_valid
     end
+
+    it 'email validation should accept valid addresses' do
+      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+      valid_addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid, "#{valid_address.inspect} should be valid"
+      end
+    end
+
+    it 'email validation should reject invalid addresses' do
+      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+      invalid_addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).to_not be_valid, "#{invalid_address.inspect} should be invalid"
+      end
+    end
   end
 
   describe 'name' do
