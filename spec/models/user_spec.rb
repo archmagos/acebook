c4a314c358b5f507b5aaf58e4c3ae8e708c6821a
John Forster <j_forster@hotmail.co.uk>
2018-07-30 18:04:01 +0100

    Added uniqueness verification, emails stored downcase

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,7 +20,8 @@ RSpec.describe User, type: :model do
     end
 
     it 'email validation should accept valid addresses' do
-      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                           first.last@foo.jp alice+bob@baz.cn]
       valid_addresses.each do |valid_address|
         @user.email = valid_address
         expect(@user).to be_valid, "#{valid_address.inspect} should be valid"
@@ -29,11 +30,26 @@ RSpec.describe User, type: :model do
 
     it 'email validation should reject invalid addresses' do
       invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                             foo@bar_baz.com foo@bar+baz.com]
       invalid_addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).to_not be_valid, "#{invalid_address.inspect} should be invalid"
       end
+    end
+
+    it 'should be unique' do
+      duplicate_user = @user.dup
+      duplicate_user.email = @user.email.upcase
+      @user.save
+      expect(duplicate_user).not_to be_valid
+    end
+
+    it 'should turn to downcase' do
+      upcase_email = 'UPCASE@DOMAIN.COM'
+      @user.email = 'UPCASE@DOMAIN.COM'
+      @user.save
+      @user.reload
+      expect(@user.email).to eq(upcase_email.downcase)
     end
   end
 
