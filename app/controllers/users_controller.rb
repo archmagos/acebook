2cd2578ed26e0b0cfe58b0111163c30cf0c49fa2
Layth <laythq@gmail.com>
2018-07-31 15:16:00 +0100

    Modified Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,27 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    # (conventional) user_params returns appropriate initialization hash,
+    # as defined in private method below...
+    @user = User.new(user_params)
+    if @user.save
+      # equivalent to "redirect_to user_url (@user)"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+
 end
