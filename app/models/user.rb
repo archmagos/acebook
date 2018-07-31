e2c595b2fb16a545e7468df745ae32c7334cdc0b
Layth <laythq@gmail.com>
2018-07-31 10:33:44 +0100

    Add password_digest column to users table, add #has_secure_password method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_REGEX },
-                    uniqueness: { case_sensitive: false }
+  format: { with: VALID_REGEX }
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180730164035) do
+ActiveRecord::Schema.define(version: 20180731092937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20180730164035) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.string "password_digest"
   end
 
 end
