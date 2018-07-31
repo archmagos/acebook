b60c9030138036cade70010166fd50c96807c53a
Layth <laythq@gmail.com>
2018-07-31 13:51:56 +0100

    Add users routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :users
   resources :posts
 end
