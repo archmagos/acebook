5bfd07d10a85bf106b9f7a5d7636e33c658fdebd
Layth <laythq@gmail.com>
2018-07-31 15:41:38 +0100

    Successful signup leads to User view

---
+++ spec/models/user_signup_spec.rb
@@ -0,0 +1,15 @@
+# require 'rails_helper'
+#
+# RSpec.describe User, type: :model do
+#   before(:each) do
+#     @user = User.new(name:'John', email: 'j_forster@hotmail.co.uk',
+#     password: 'password', password_confirmation: 'password')
+#   end
+#
+#   describe 'valid submission' do
+#     it 'should add a user to the database' do
+#
+#     end
+#   end
+#
+#   end
