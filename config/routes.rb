d906bf8e5e22c2e2d4058eef66b64161ec5af67e
John Forster <j_forster@hotmail.co.uk>
2018-08-01 16:55:23 +0100

    Added session controller and login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   get  '/signup',  to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
