a40a160a31139b7af981033d7c1f98206eb1b235
John Forster <j_forster@hotmail.co.uk>
2018-08-02 10:06:04 +0100

    cleaned up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'sessions/new'
-
-  get 'users/new'
-
-  get  '/signup',  to: 'users#new'
-
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/new' # Is this line needed?
+  get     '/signup',  to: 'users#new'
+  get     '/login',   to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
   resources :users
   resources :posts
 end
