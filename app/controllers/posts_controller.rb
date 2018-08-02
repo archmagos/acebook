3ea75da94a80860a2fdb1dfb9fb7873becec317f
Benjamin <ben.smith.gordon@gmail.com>
2018-08-02 11:18:45 +0100

    More corrections

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,23 +2,22 @@
 
 # No comment
 class PostsController < ApplicationController
-
-def new
-    @post = Post.new
+  def new
+  @post = Post.new
 end
 
 def create
-    @post = Post.create(post_params)
-    redirect_to posts_url
+  @post = Post.create(post_params)
+  redirect_to posts_url
 end
 
 def index
-    @posts = Post.all.order('created_at DESC')
+  @posts = Post.all.order('created_at DESC')
 end
 
   private
 
 def post_params
-    params.require(:post).permit(:message)
-  end
+  params.require(:post).permit(:message)
+end
 end
