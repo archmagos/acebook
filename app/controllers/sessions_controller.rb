597572970105536aa9a00c9a138d388587ae68a7
John Forster <j_forster@hotmail.co.uk>
2018-08-02 10:02:37 +0100

    Edited sessions_controller.rb for rubocop completeness

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
+# The rails controller for handling sessions.
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       # log_in method pre-defined in SessionsHelper module
       log_in user
       # rails converts user to user_url(user)
       redirect_to user
     else
       # flash error message telling user to try again...
-    render 'new'
-  end
+      render 'new'
+    end
   end
 
   def destroy
