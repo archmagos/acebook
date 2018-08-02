e8c6c32e8527478e74e9a0aa536d3c6b9a0b595f
Benjamin <ben.smith.gordon@gmail.com>
2018-08-02 11:10:59 +0100

    Added Frozen strings

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,22 +2,23 @@
 
 # No comment
 class PostsController < ApplicationController
-  def new
-    @post = Post.new
-  end
 
-  def create
+def new
+    @post = Post.new
+end
+
+def create
     @post = Post.create(post_params)
     redirect_to posts_url
-  end
+end
 
-  def index
+def index
     @posts = Post.all.order('created_at DESC')
-  end
+end
 
   private
 
-  def post_params
+def post_params
     params.require(:post).permit(:message)
   end
 end
