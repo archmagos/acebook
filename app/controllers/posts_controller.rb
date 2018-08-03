3b19f755d98fe7e115bccc485df6159adf77c7a7
Fred <fredch87@outlook.com>
2018-08-03 16:09:56 +0100

    Delete button working and present on posts page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,6 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.order('created_at DESC')
-    @likeid = Like.select('id').where(post_id: 1, user_id: 1).first.id
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,8 @@ class Post < ApplicationRecord
   def poster_name
     User.find(user_id).name if user_id
   end
+
+  def like_id(post_id)
+    Like.select('id').where(post_id: post_id, user_id: 1).first&.id
+  end
 end
