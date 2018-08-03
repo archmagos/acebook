9c70e76a8cb027986dc1ce27644fa74c8c19b03f
John Forster <j_forster@hotmail.co.uk>
2018-08-03 11:30:18 +0100

    added user creation if it does not exist

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
+require 'pg'
 
 RSpec.describe PostsController, type: :controller do
   before(:each) do
     user_password = '1234567890'
+    unless User.find_by(email: 'john@forster.com')
+      User.create(
+        name: 'John', email: 'john@forster.com',
+        password: user_password, password_confirmation: user_password
+      )
+    end
     @controller = SessionsController.new
     post :create, params: {
       session: { email: 'john@forster.com', password: user_password }
