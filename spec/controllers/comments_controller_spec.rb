cc8cdab64d57fe0c615c7a1119decab5c5b5ffb3
Benjamin <ben.smith.gordon@gmail.com>
2018-08-04 15:16:29 +0100

    Test Created

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-
+  describe "POST /" do
+    it "responds with 200" do
+      Post.create({message: "Hello, world!", id: 1})
+      post :create, params: { comment: {body: "Hello, world!"}, post_id: 1}
+      expect(response).to redirect_to(posts_url)
+    end
+  end
 end
 
