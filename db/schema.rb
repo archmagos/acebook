d0fe4bf0f56f32ff01acd9a06b863b0b44e54223
Benjamin <ben.smith.gordon@gmail.com>
2018-08-04 16:13:30 +0100

    Fixed broken migration

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180802154024) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
