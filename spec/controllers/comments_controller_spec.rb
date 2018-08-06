4933690ec63a1b9a09b687a789c44e5db3b77362
John Forster <j_forster@hotmail.co.uk>
2018-08-06 11:36:43 +0100

    Fixed failing test

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe CommentsController, type: :controller do
   end
   describe "POST /" do
     it "responds with 200" do
-      Post.create({message: "Hello, world!", id: 1})
+      test_post = Post.create({message: "Hello, world!", id: 1})
       post :create, params: { comment: {body: "Hello, world!"}, post_id: 1}
-      expect(response).to redirect_to(posts_url)
+      expect(response).to redirect_to(posts_url(test_post.id))
     end
   end
 end
