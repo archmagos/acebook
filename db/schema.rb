a7e2bab34541f3cd03f2b62525521fe3a6248bd6
John Forster <j_forster@hotmail.co.uk>
2018-08-06 11:42:57 +0100

    Updated schema file

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20180806103122) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
