217acaec1608243a02465159ce9fe0b5f1c07b1e
John Forster <j_forster@hotmail.co.uk>
2018-08-06 11:52:49 +0100

    All tests passing, foreign key ready

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180806105005) do
+ActiveRecord::Schema.define(version: 20180806103122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
+
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "post_id"
