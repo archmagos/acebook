df138c6d9362500d9eb54d43d957fa77459683da
John Forster <j_forster@hotmail.co.uk>
2018-08-08 16:10:15 +0100

    Added config to allow CORS

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,14 @@ module Acebook
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'http://localhost:4000',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
