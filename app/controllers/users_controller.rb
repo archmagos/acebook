a79eaf9b11667ddfaf10656e42baae0b0adb52b6
Layth <laythq@gmail.com>
2018-08-08 11:16:00 +0100

    Removed login helpers from Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,17 @@
 class UsersController < ApplicationController
   # Not sure whether to keep this or not?
   def show
-    @logged_in = logged_in
-    @user = User.find(params[:id])
+    # @logged_in = logged_in
+    # @user = User.find(params[:id])
   end
 
   def new
-    @user = User.new
+    # @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.create(user_params)
     if @user.save
-      log_in @user
       all = User.all
       render json: all.last.to_json
     else
